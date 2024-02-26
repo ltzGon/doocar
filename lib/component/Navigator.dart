dec54f884f0fa887f50ee05b0c447aae7ce8a48e
Surachaizxzx <surachaikai2545@gmail.com>
2024-02-26 23:47:01 +0700

    shop

---
+++ lib/component/Navigator.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:rolling_bottom_bar/rolling_bottom_bar.dart';
+import 'package:rolling_bottom_bar/rolling_bottom_bar_item.dart';
+import 'package:flutter/material.dart';
+import '../screen/Pay_screen.dart';
+import '../screen/buycar_screen.dart';
+import '../screen/home_screen.dart';
+import '../screen/setting_screen.dart';
+
+class Navigatorbar extends StatefulWidget {
+  const Navigatorbar({super.key});
+
+  @override
+  State<Navigatorbar> createState() => _NavigatorbarState();
+}
+
+class _NavigatorbarState extends State<Navigatorbar> {
+  final _pageControlller = PageController();
+  @override
+  void dispose() {
+    super.dispose();
+    _pageControlller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageControlller,
+        children: const <Widget>[
+          Homescreen(),
+          BuyMycar(),
+          Pay_in_installments(),
+          SettingProfile(),
+        ],
+      ),
+      extendBody: true,
+      bottomNavigationBar: RollingBottomBar(
+        color: Color.fromARGB(226, 163, 161, 163),
+        controller: _pageControlller,
+        flat: true,
+        useActiveColorByDefault: false,
+        items: const [
+          RollingBottomBarItem(Icons.shopping_cart,
+              label: 'Shop', activeColor: Colors.redAccent),
+          RollingBottomBarItem(
+            Icons.car_rental,
+            label: 'Mycar',
+            activeColor: Colors.blueAccent,
+          ),
+          RollingBottomBarItem(Icons.attach_money,
+              label: 'Pay', activeColor: Colors.green),
+          RollingBottomBarItem(
+            Icons.settings,
+            label: 'Setting',
+            activeColor: Color.fromARGB(255, 10, 10, 10),
+          ),
+        ],
+        enableIconRotation: true,
+        onTap: (index) {
+          _pageControlller.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 400),
+            curve: Curves.fastOutSlowIn,
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,16 @@
-import 'package:doocar/screen/setting_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:rolling_bottom_bar/rolling_bottom_bar.dart';
-import 'package:rolling_bottom_bar/rolling_bottom_bar_item.dart';
+import 'component/Navigator.dart';
 
-import 'screen/Pay_screen.dart';
-import 'screen/buycar_screen.dart';
-import 'screen/home_screen.dart';
-
-void main() => runApp(NavigationBarApp());
+void main() => runApp(const NavigationBarApp());
 
 class NavigationBarApp extends StatelessWidget {
-  NavigationBarApp({super.key});
-  final _pageControlller = PageController();
-  @override
-  void dispose() {
-    _pageControlller.dispose();
-  }
+  const NavigationBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: PageView(
-          controller: _pageControlller,
-          children: const <Widget>[
-            Homescreen(),
-            BuyMycar(),
-            Pay_in_installments(),
-            SettingProfile(),
-          ],
-        ),
-        extendBody: true,
-        bottomNavigationBar: RollingBottomBar(
-          color: Color.fromARGB(160, 195, 230, 119),
-          controller: _pageControlller,
-          flat: true,
-          useActiveColorByDefault: false,
-          items: const [
-            RollingBottomBarItem(Icons.home,
-                label: 'Shop', activeColor: Colors.redAccent),
-            RollingBottomBarItem(Icons.car_rental,
-                label: 'Mycar', activeColor: Colors.blueAccent),
-            RollingBottomBarItem(Icons.person,
-                label: 'Pay', activeColor: Colors.green),
-            RollingBottomBarItem(Icons.settings,
-                label: 'Setting', activeColor: Color.fromARGB(255, 10, 10, 10)),
-          ],
-          enableIconRotation: true,
-          onTap: (index) {
-            _pageControlller.animateToPage(
-              index,
-              duration: const Duration(milliseconds: 400),
-              curve: Curves.fastOutSlowIn,
-            );
-          },
-        ),
-      ),
+      home: Navigatorbar(),
     );
   }
 }
